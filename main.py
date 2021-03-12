f5935518868e84db93e11b48667a56eea8e6d6fa
unknown <angelibustria@gmail.com>
2021-03-12 14:26:50 +0800

    PSB Battle Game

--- main.py
+++ main.py
@@ -28,7 +28,17 @@ if chosenMenu=="N":
                 characterOne = str(input("Enter chosen character:"))
                 def playerOne(characterOne):
                     if characterOne=="T":
-                        player1unit1 = "Troll"
+                        # player1unit1 = "Troll"
+                        trollFile = open("init/troll.json", "r")
+                        trollJson = trollFile.read()
+                        troll = json.loads(trollJson)
+                        troll["name"] = str(input("Name your character:"))
+                        configFile = open("config.json","r")
+                        configJson = configFile.read()
+                        config = json.loads(configJson)
+                        config["player_one"]["team"].append(troll)
+                        with open('config.json','w') as outfile:
+                            json.dump(config, outfile)
                     elif characterOne=="K":
                         # player1unit1 = "Knight"
                         knightFile = open("init/knight.json", "r")
