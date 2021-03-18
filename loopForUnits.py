91c7dda0cba5d19f63edeb7e39ccc38b6ccb422f
unknown <angelibustria@gmail.com>
2021-03-18 13:14:08 +0800

    PSB Battle Game

--- loopForUnits.py
+++ loopForUnits.py
@@ -1,34 +1,22 @@
 playerOneNumberOfUnits = int(input("How many units in your team?:"))
 
-playerOneListOfUnits = {"character_class":[], "character_name":[]}
-
+playerOneListOfUnits = []
 
 def playerOneCharacters (playerOneNumberOfUnits):
     count = 0
     
-    while count == 0:
-        print (1)
+    while count < playerOneNumberOfUnits :
+        print(count + 1)
         print ("Choose a character for your team")
         print ("""[T]roll - A strong attack but careless monster that has 50% chance of missing its target\n[K]night - A soldier with decent attack and strong defence\n[M]age - A spell caster with low attack/defence""")
         characterClassInput = str(input("Enter chosen character:"))
-        playerOneListOfUnits["character_class"].append(characterClassInput)
         characterNameInput = str(input("Enter character's name:"))
-        playerOneListOfUnits["character_name"].append(characterNameInput)
+        localplayerOneListOfUnits = {"character_class":[characterClassInput], "character_name":[characterNameInput]}
+        playerOneListOfUnits.append(localplayerOneListOfUnits)
         count = count + 1
-        print(count + 1)
-        if count < playerOneNumberOfUnits:
-            print ("Choose a character for your team")
-            print ("""[T]roll - A strong attack but careless monster that has 50% chance of missing its target\n[K]night - A soldier with decent attack and strong defence\n[M]age - A spell caster with low attack/defence""")
-            playerOneExtendedCharacters = {"character_class":[], "character_name":[]}
-            characterClassInput = str(input("Enter chosen character:"))
-            playerOneExtendedCharacters["character_class"].append(characterClassInput)
-            characterNameInput = str(input("Enter character's name:"))            
-            playerOneExtendedCharacters["character_name"].append(characterNameInput)
-            finalListOFPlayerOneUnits = {**playerOneListOfUnits,**playerOneExtendedCharacters}
-            count = count + 1
-            print ("Player 1 Unit:")
-            print (finalListOFPlayerOneUnits)
+    
+
 
 playerOneCharacters (playerOneNumberOfUnits)
 
-print (playerOneListOfUnits)
+print (list(playerOneListOfUnits))
